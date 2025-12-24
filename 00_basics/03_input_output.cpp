11ae565039dcd933cc5896ad1f4deb9b904efa45
Mohammad Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2025-12-24 16:15:14 +0600

    input and output operation in c plus plus

---
+++ 00_basics/03_input_output.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int age;
+  // output ➡️ will be show in console
+  cout << "Enter Your Age: ";
+  // input
+  cin >> age; // ⬅️ takes input from the user
+  cout << "Your Age is " << age << endl;
+  return 0;
+}
