259109265232faf7de0c848aa4d886e0c2599693
Mohammad Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2025-12-24 16:07:50 +0600

    learn about variables in c plus plus

---
+++ 00_basics/02_variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  // 1️⃣ integer
+  int a = 5; // integer value
+  // 2️⃣ float
+  float b = 3.1415; // decimal value
+  // 3️⃣ double
+  double c = 3.14151719; // more precise decimal
+  // 4️⃣ char
+  char d = 'A'; // single character
+  // 5️⃣ boolean
+  bool e = true; // true or false
+  // 6️⃣ string
+  string f = "hello"; // sequence of characters
+  return 0;
+}
