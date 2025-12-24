9dcc5183b86bae67d3f27cb48a7e48fdf7ae0282
Mohammad Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2025-12-24 15:57:44 +0600

    basic structure of c++ program

---
+++ 00_basics/basics.cpp
@@ -0,0 +1,9 @@
+#include <iostream>  // library inclusion
+using namespace std; // avoid wring std:: cout,cin
+
+// entry point of the program
+int main()
+{
+  cout << "hello world" << endl; // print text on console
+  return 0;                      // end of program
+}
